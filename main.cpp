a2bea7bb2b7b25f10607e9287e96165f1d04704f
Thomas Röggla <t.roggla@cwi.nl>
2024-04-13 19:16:36 +0200

    added keymap for keypad

--- main.cpp
+++ main.cpp
@@ -11,6 +11,22 @@
 #define NUM_ROWS 5
 #define NUM_COLS 4
 
+// Keymap definition
+const char keys[NUM_ROWS][NUM_COLS] = {
+  {'x', 'x', 'x', 'x'},
+  {'7', '8', '9', '/'},
+  {'4', '5', '6', '*'},
+  {'1', '2', '3', '-'},
+  {'0', '.', '=', '+'}
+};
+
+// Pins for keypad rows and columns
+byte rowPins[NUM_ROWS] = {12, 9, 8, 7, 6};
+byte colPins[NUM_COLS] = {5, 4, 3, 2};
+
+// Initialise hardware devices
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, NUM_ROWS, NUM_COLS);
+
 void setup() {
 }
 
