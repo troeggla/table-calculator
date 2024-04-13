e7eac90ba1d3a7dd5ee47c638457a5346448047b
Thomas Röggla <t.roggla@cwi.nl>
2024-04-13 19:10:52 +0200

    added pin definitions for LCD screen and keypad

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 #include <Keypad.h>
 #include <U8g2lib.h>
 
+// Pins for LCD screen
+#define LCD_CLK 13
+#define LCD_DATA 11
+#define LCD_CS 10
+
+// Number of rows and columns of keypad
+#define NUM_ROWS 5
+#define NUM_COLS 4
+
 void setup() {
 }
 
